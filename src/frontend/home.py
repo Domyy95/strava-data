2528066fd380bf4068bc70b38f2370574d7ef96a
Dominic Crippa <dominic.crippa@gmail.com>
2024-07-10 22:53:22 +0200

    fix: fix multiple settings loading

--- src/frontend/home.py
+++ src/frontend/home.py
@@ -8,10 +8,7 @@ def page2():
     st.title("Second page")
 
 
-def home(strava_api: StravaAPI):
-    if "strava_api" not in st.session_state:
-        st.session_state["strava_api"] = strava_api
-
+def home(strava_api: StravaAPI = None):
     map_page = st.Page(f"{front_end_dir}/map.py", title="Map", icon=":material/map:")
     test_page = st.Page(page2, title="test")
 
--- src/main.py
+++ src/main.py
@@ -1,12 +1,20 @@
+import streamlit as st
 from src.api.strava import StravaAPI
 from src.settings import get_settings
 from src.frontend.home import home
 
 
+@st.cache_data
+def load_settings():
+    return get_settings()
+
+
 def run():
-    settings = get_settings()
-    strava_api = StravaAPI(settings.access_token)
-    home(strava_api)
+    settings = load_settings()
+    if "strava_api" not in st.session_state:
+        st.session_state["strava_api"] = StravaAPI(settings.access_token)
+
+    home()
 
 
 if __name__ == "__main__":
