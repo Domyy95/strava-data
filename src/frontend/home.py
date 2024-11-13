9502bbd69f53cd0e6bd86f1e93a8bc6eba94f5cb
Dominic Crippa <dominic.crippa@gmail.com>
2024-11-13 10:34:26 +0100

    refactor: refactoring home

--- src/frontend/home.py
+++ src/frontend/home.py
@@ -4,10 +4,12 @@ front_end_dir = "./frontend/pages/"
 
 
 def home():
-    personal_stats = st.Page(f"{front_end_dir}/user_stats.py", title="Personal Stats", icon="👤")
-    run_comparison = st.Page(f"{front_end_dir}/compare.py", title="Compare Runs", icon="⏱️")
+    # Page to add your credentials that unlock all the other pages --> This file
+    user_stats = st.Page(f"{front_end_dir}/user_stats.py", title="Personal Stats", icon="👤")
+    run_comparison = st.Page(f"{front_end_dir}/compare.py", title="Compare Your Runs", icon="⏱️")
     map_page = st.Page(f"{front_end_dir}/map.py", title="Segment Map Search", icon="🗺️")
-    # other_user_stats = st.Page(f"{front_end_dir}/other_user_stats.py", title="Other User Stats", icon="👥")
+    # my_koms = st.Page(f"{front_end_dir}/my_koms.py", title="My Koms", icon="")
+    # last_activities = st.Page(f"{front_end_dir}/last_activities.py", title="Last Activities", icon="")
 
-    pg = st.navigation([personal_stats, run_comparison, map_page])
+    pg = st.navigation([user_stats, run_comparison, map_page])  #  my_koms, last_activities
     pg.run()
